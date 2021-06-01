6f4bc36aaa85a686e883380ae2776ae86c9f4508
Mark M <otteromkram@gmail.com>
2021-06-01 09:33:28 -0500

    added helper function for testing

--- go/main.go
+++ go/main.go
@@ -5,26 +5,42 @@ import (
 	"fmt"
 )
 
-// Test data
-var squarePolygon p.Polygon = p.Polygon{
-	{X:0, Y: 0},
-	{X:10, Y: 0},
-	{X:10, Y: 10},
-	{X:0, Y: 10},		
-}
 
 func main() {
-	// Finding centroid demo for two-dimensional polygon.
-	// The initial run uses a square
+	/////////
+	//! Test shapes
+	var squarePolygon = &p.Polygon{
+		{X:0, Y: 0},
+		{X:10, Y: 0},
+		{X:10, Y: 10},
+		{X:0, Y: 10},		
+	}
+
+	var pentagonPolygon = &p.Polygon{
+		{X:100, Y: 0},
+		{X:200, Y: 77},
+		{X:160, Y: 200},
+		{X:40, Y: 200},		
+		{X:0, Y: 77},
+	}	
 
 
-	// fmt.Printf("Pre-run centroid is: (%.6f, %.6f)\n", p.Centroid.X, p.Centroid.Y)
-	fmt.Printf("The polygon length is: %d\n", len(squarePolygon))
-	fmt.Printf("The polygon capacity is: %d\n", cap(squarePolygon))
-	fmt.Printf("The vertex count is: %d\n", squarePolygon.VertexCount())
+	RunPolygonTest(squarePolygon, "Square")
+	RunPolygonTest(pentagonPolygon, "Pentagon")
 
-	squarePolygon.CalculateCentroid()
+}
 
+
+//! Print results to console.
+func RunPolygonTest(pg *p.Polygon, shapeName string) {
+	fmt.Println("Finding centroid for a ", shapeName)
+
+	fmt.Printf("The polygon length is: %d\n", len(*pg))
+	fmt.Printf("The polygon capacity is: %d\n", cap(*pg))
+	fmt.Printf("The vertex count is: %d\n", pg.VertexCount())
+
+	pg.CalculateCentroid()
+	
 	fmt.Printf("Post-run centroid is: (%.6f, %.6f)\n", p.Centroid.X, p.Centroid.Y)
 	fmt.Printf("Area is: %f\n", p.SignedArea)
 }
